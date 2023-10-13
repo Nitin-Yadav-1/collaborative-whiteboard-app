18b987646083cedb266afb21efdda564dc478ba7
Nitin Yadav <nitinyadav3720@gmail.com>
2023-10-13 22:46:18 +0530

    add tests

---
+++ tests/test_auth.py
@@ -0,0 +1,36 @@
+
+from fastapi import status
+from fastapi.testclient import TestClient
+from src.app import app
+
+
+client = TestClient(app)
+
+
+def test_login_invalid_email():
+  res = client.post(
+    "/login",
+    json={"email": "not-exists@gmail.com", "password": "password"}
+  )
+  assert res.status_code == status.HTTP_401_UNAUTHORIZED
+  assert res.json() == {"detail" : "Invalid Credentials"}
+
+
+def test_login_invalid_password():
+  res = client.post(
+    "/login",
+    json={"email": "test@gmail.com", "password": ""}
+  )
+  assert res.status_code == status.HTTP_401_UNAUTHORIZED
+  assert res.json() == {"detail" : "Invalid Credentials"}
+
+
+def test_login_valid():
+  res = client.post(
+    "/login",
+    json={"email": "test@gmail.com", "password": "password"}
+  )
+  assert res.status_code == status.HTTP_200_OK
+  data = res.json()
+  assert len(data) == 1
+  assert "token" in data
